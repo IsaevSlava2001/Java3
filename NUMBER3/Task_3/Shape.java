f21037ec37b3db21f10488d2d839131c284b7e7d
Trebon_Deon <IsaevSlava.2001@yandex.ru>
2022-12-16 19:31:35 +0300

    upd

---
+++ NUMBER3/Task_3/Shape.java
@@ -0,0 +1,43 @@
+abstract public class Shape
+{
+    protected String Color;
+    protected boolean Filled;
+    public void Shape()
+    {
+
+    }
+    public void Shape(String color, boolean filled)
+    {
+        this.Filled=filled;
+        this.Color=color;
+    }
+    public String getColor()
+    {
+        return Color;
+    }
+    public void setColor(String color)
+    {
+        this.Color=color;
+    }
+    public boolean isFilled()
+    {
+        return Filled;
+    }
+    public void setFilled(boolean filled)
+    {
+        this.Filled=filled;
+    }
+    public double getArea()
+    {
+        return 0;
+    }
+    public double getPerimeter()
+    {
+        return 0;
+    }
+    public String toString()
+    {
+        return "Color: "+ this.Color+", isFilled: "+Filled;
+    }
+
+}
